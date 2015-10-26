17550a0dacb005366d31a836564e4450121c254c
secret_online <me+github@secretonline.co>
2015-10-27 12:23:12 +1300

    Move things around a little

--- info.js
+++ info.js
@@ -1,7 +1,42 @@
-var tabs = document.querySelector('paper-tabs');
-var pages = document.querySelector('iron-pages');
+(function(win, doc) {
+  'use strict';
 
-tabs.addEventListener('iron-select', function() {
-  pages.selected = tabs.selected;
-});
-pages.select(0);
+  function httpGet(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.addEventListener('load', function() {
+      callback(xhr.responseText);
+    });
+    xhr.open('get', url, true);
+    xhr.send();
+  }
+
+  var tabs = doc.querySelector('paper-tabs');
+  var pages = doc.querySelector('iron-pages');
+
+  tabs.addEventListener('iron-select', function() {
+    pages.selected = tabs.selected;
+  });
+  tabs.select(0);
+
+  httpGet('data/info.json', function(response) {
+    var cardList = doc.querySelector('.info-list');
+    var data = JSON.parse(response);
+
+    data.forEach(function(item) {
+      var card = doc.createElement('paper-card');
+      card.heading = item.title;
+
+      var content = doc.createElement('div');
+      content.classList.add('card-content');
+      item.text.forEach(function(text) {
+        var t = doc.createElement('p');
+        t.textContent = text;
+        content.appendChild(t);
+      });
+      card.appendChild(content);
+
+      cardList.appendChild(card);
+    });
+  });
+
+})(window, document);
