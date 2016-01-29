a40e56b40e6dcc309a0ce4d9ee6b3c372b430b79
secret_online <me+github@secretonline.co>
2016-01-30 09:43:27 +1300

    Move GA outside init
    Happens as soon as script it loaded, not on DOMContentLoaded event

--- info.js
+++ info.js
@@ -110,23 +110,6 @@
       }
     }
 
-    // Google Analytics
-    // No personal information stored, just how it's used
-    (function(i, s, o, g, r, a, m) {
-      i.GoogleAnalyticsObject = r;
-      i[r] = i[r] || function() {
-        (i[r].q = i[r].q || []).push(arguments);
-      };
-      i[r].l = 1 * new Date();
-      a = s.createElement(o);
-      m = s.getElementsByTagName(o)[0];
-      a.async = 1;
-      a.src = g;
-      m.parentNode.insertBefore(a, m);
-    })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
-    ga('create', 'UA-55893575-6', 'auto');
-    ga('send', 'pageview');
-
     // Create cards for all the information
     getCategories();
     getResources();
@@ -1462,6 +1445,23 @@
     };
     throttle("scroll", "optimizedScroll");
   })();
+
+  // Google Analytics
+  // No personal information stored, just how it's used
+  (function(i, s, o, g, r, a, m) {
+    i.GoogleAnalyticsObject = r;
+    i[r] = i[r] || function() {
+      (i[r].q = i[r].q || []).push(arguments);
+    };
+    i[r].l = 1 * new Date();
+    a = s.createElement(o);
+    m = s.getElementsByTagName(o)[0];
+    a.async = 1;
+    a.src = g;
+    m.parentNode.insertBefore(a, m);
+  })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+  ga('create', 'UA-55893575-6', 'auto');
+  ga('send', 'pageview');
 
   if (doc.readyState !== 'loading')
     initInfo();
